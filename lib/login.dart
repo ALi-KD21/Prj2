8c8cda1589a5248611e9b2c9f3d6bc704d45971a
USER <alisrk487@gmail.com>
2023-12-29 23:44:33 +0200

    finished project

--- lib/login.dart
+++ lib/login.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
-import 'menu.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
+import 'home.dart';
+import 'add_category.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class Login extends StatefulWidget {
+  const Login({super.key});
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<Login> createState() => _HomeState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+class _HomeState extends State<Login> {
+  final TextEditingController _controller = TextEditingController(); // hold user key from TextField
+  final EncryptedSharedPreferences _encryptedData =
+  EncryptedSharedPreferences(); // used to store the key later
+
+  // this function opens the Add Category page, if we managed to save key successfully
+  void update(bool success) {
+    if (success) { // open the Add Category page if successful
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const Home()));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('failed to set key')));
+    }
+  }
+  // adds key to EncryptedSharedPreferences
+  checkLogin() async {
+    // make sure the key is not empty
+    if (_controller.text.toString().trim() == '') {
+      update(false);
+    } else {
+      // attempt to save key. Saving the key and encrypting it takes time.
+      // so it is done asynchronously
+      _encryptedData
+          .setString('myKey', _controller.text.toString())
+          .then((bool success) { // then is equivalent to using wait
+        if (success) {
+          update(true);
+        } else {
+          update(false);
+        }
+      });
+    }
+  }
+
+  // opens the Add Category page, if the key exists. It is called when
+  // the application starts
+  void checkSavedData() async {
+    _encryptedData.getString('myKey').then((String myKey) {
+      if (myKey.isNotEmpty) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(
+            builder: (context) => const AddCategory()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // call the below function to check if key exists
+    checkSavedData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,84 +70,24 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Login Page'),
         centerTitle: true,
-        backgroundColor: Colors.teal[300],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/rm222-mind-14.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-                child: const CircleAvatar(
-                  backgroundImage: AssetImage('images/z.jpg'),
-                  radius: 50,
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  icon: Icon(Icons.person),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  icon: Icon(Icons.lock),
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  String username = _usernameController.text;
-                  String password = _passwordController.text;
-
-                  if (username == 'Mohammad' && password == 'Zahran') {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => const HomePage(title: 'Zizo Tech')),
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal[300],
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Center(
+          child: Column(children: [
+            const SizedBox(height: 10),
+            SizedBox(
+                width: 200,
+                child: TextField(
+                  // replace typed text with * for passwords
+                  obscureText: true,
+                  enableSuggestions: false, // disable suggestions for password
+                  autocorrect: false, // disable auto correct for password
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Enter Key'),
+                )),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: checkLogin, child: const Text('Save'))
+          ])),
     );
   }
 }
